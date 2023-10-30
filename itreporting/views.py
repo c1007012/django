47cfc37844f76f97f227f94428886ce83be1609b
c1007012 <c1007012@hallam.shu.ac.uk>
2023-10-30 17:21:24 +0000

    Updated Changes -StyleSheet and base.html 30/10/23

--- itreporting/views.py
+++ itreporting/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 
 def home(request): 
 
-    return render(request, 'itreporting/home.html')
+   return render(request, 'itreporting/home.html', {'title': 'Welcome'})
 
 def about(request):
 
-    return render(request, 'itreporting/about.html')
+    return render(request, 'itreporting/about.html', {'title': 'About'})
 
 def contact(request):
 
-    return render(request, 'itreporting/contact.html')
+    return render(request, 'itreporting/contact.html', {'title': 'Contact Us'})
 
 # Create your views here.
