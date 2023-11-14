f74dc28f3ed673f939da52c77ff0859e388b4a75
c1007012 <c1007012@hallam.shu.ac.uk>
2023-11-14 13:16:06 +0000

    Week 7 Work

--- itreporting/urls.py
+++ itreporting/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 
 app_name = 'itreporting'
 
@@ -8,4 +9,12 @@ urlpatterns = [
     path('', views.home, name = 'home'),
     path('about', views.about, name='about'),
     path('contact', views.contact, name='contact'),
+    path('report', PostListView.as_view(), name = 'report'),
+    path('issue/<int:pk>', PostDetailView.as_view(), name = 'issue-detail'),
+    path('issue/new', PostCreateView.as_view(), name = 'issue-create'),
+    path('issue/<int:pk>/update/', PostUpdateView.as_view(), name = 'issue-update'),
+    path('issue/<int:pk>/delete/', PostDeleteView.as_view(), name = 'issue-delete'),
+    path('issue/<str:username>', UserPostListView.as_view(), name = 'user-issues'),
+
+
 ]
